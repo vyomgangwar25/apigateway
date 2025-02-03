ce159cd09bf3361686ad2571f328ee03fbe8e585
vyomgangwar25 <vyomgangwar25@gmail.com>
2025-02-03 19:54:31 +0530

    api gateway service

---
+++ src/main/java/com/ics/gateway/GatewayApplication.java
@@ -0,0 +1,28 @@
+package com.ics.gateway;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Override 
+	public void run(String... args) throws Exception {
+		System.out.println("running api gateway");
+	}
+
+//	@Bean
+//	 RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder) {
+//		return routeLocatorBuilder.routes().route("api_gateway", r -> r.path("/user/**").uri("http://localhost:8081/"))
+//				.build();
+//	}
+
+}
